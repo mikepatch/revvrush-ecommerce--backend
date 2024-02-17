2d2e43a3da96019f1658abb820c825d91e5d64eb
Michal Lata <michallata.dev@gmail.com>
2024-02-17 11:20:11 +0100

    feat: Add PORT env variable, implement logger inside app, add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const logger = new Logger('bootstrap');
+  app.useLogger(logger);
+
+  await app.listen(PORT);
+  logger.log(`Application is running on: http://localhost:${PORT} 🚀`);
 }
 bootstrap();
