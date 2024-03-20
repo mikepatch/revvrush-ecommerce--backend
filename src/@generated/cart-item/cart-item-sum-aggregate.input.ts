a35a99755cd978ef42875c2043becea95d800129
Michal Lata <michallata.dev@gmail.com>
2024-03-20 19:59:13 +0100

    patch: Update prisma @generated

--- src/@generated/cart-item/cart-item-sum-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class CartItemSumAggregateInput {
-
-    @Field(() => Boolean, {nullable:true})
-    quantity?: true;
-}
