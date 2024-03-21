07f30937ff093d317817a5ffa9c7f6b39b4559fb
Michal Lata <michallata.dev@gmail.com>
2024-03-21 19:30:25 +0100

    feat: Import PrismaService in OrdersModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { OrdersService } from './orders.service';
 import { OrdersResolver } from './orders.resolver';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Module({
-  providers: [OrdersResolver, OrdersService]
+  providers: [OrdersResolver, OrdersService, PrismaService],
 })
 export class OrdersModule {}
